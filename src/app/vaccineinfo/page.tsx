3ccee7fd87f52180799ce940b858ee529d6bff74
seokwon <tjrtkd95@gmail.com>
2024-10-23 17:06:03 +0900

    update: 백신리스트 fetch 함수 생성 및 상태 저장

--- src/app/vaccineinfo/page.tsx
+++ src/app/vaccineinfo/page.tsx
@@ -1,5 +1,45 @@
-const page = () => {
-  return <div>접종 정보 페이지</div>;
+"use client";
+
+import browserClient from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import { Tables } from "../../../database.types";
+
+type VaccineList = Tables<"vaccine">;
+
+const Page = () => {
+  const [vaccineList, setVaccineList] = useState<VaccineList[] | null>([]);
+  const [ageGroup, setAgeGroup] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+
+  const getVaccineInfo = async () => {
+    try {
+      const { data } = await browserClient.from("vaccine").select("*");
+      setVaccineList(data);
+    } catch (error) {
+      console.error(error);
+      throw new Error(`Error : ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getVaccineInfo();
+  }, []);
+
+  return (
+    <div>
+      접종 정보 페이지
+      <div className="flex flex-wrap gap-3">
+        {vaccineList?.map((item) => {
+          return (
+            <div key={item.id} className="border-[1px] gap-2 w-56 p-3">
+              <p>{item.disease_name}</p>
+              {item.description}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
