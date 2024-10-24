44bd3e7b939848f105c0f4c5066a9e28743692f5
stopbrother <stopbro0323@naver.com>
2024-10-24 17:50:46 +0900

    update: vaccine db SSR로 미리 가져오기

--- src/app/child/[id]/page.tsx
+++ src/app/child/[id]/page.tsx
@@ -1,5 +1,24 @@
-const page = () => {
-  return <div>접종 체크리스트 페이지</div>;
+import VaccinateRecord from "@/components/VaccinateRecord";
+import { createClient } from "@/utils/supabase/server";
+import { getVaccines } from "@/utils/vaccinations/vaccinationsApi";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+
+const VaccinatePage = async () => {
+  const serverClient = createClient();
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["vaccine"],
+    queryFn: () => getVaccines(serverClient)
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div>
+        <VaccinateRecord />
+      </div>
+    </HydrationBoundary>
+  );
 };
 
-export default page;
+export default VaccinatePage;
