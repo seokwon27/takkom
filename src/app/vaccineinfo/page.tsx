3ea099aa43d4a54deee1d61f7226535579d7bafb
seokwon <tjrtkd95@gmail.com>
2024-10-24 20:53:40 +0900

    refactor: 서버컴포넌트 변환 및 컴포넌트 분리

--- src/app/vaccineinfo/page.tsx
+++ src/app/vaccineinfo/page.tsx
@@ -1,45 +1,20 @@
-"use client";
+import AgeFilter from "@/components/vaccineinfo/AgeFilter";
+import AgeGroup from "@/components/vaccineinfo/AgeGroup";
+import VaccineList from "@/components/vaccineinfo/VaccineList";
+import { getVaccineList } from "@/utils/vaccineInfo/server-action";
 
-import browserClient from "@/utils/supabase/client";
-import { useEffect, useState } from "react";
-import { Tables } from "../../../database.types";
-
-type VaccineList = Tables<"vaccine">;
-
-const Page = () => {
-  const [vaccineList, setVaccineList] = useState<VaccineList[] | null>([]);
-  const [ageGroup, setAgeGroup] = useState<string>("");
-  const [age, setAge] = useState<string>("");
-
-  const getVaccineInfo = async () => {
-    try {
-      const { data } = await browserClient.from("vaccine").select("*");
-      setVaccineList(data);
-    } catch (error) {
-      console.error(error);
-      throw new Error(`Error : ${error}`);
-    }
-  };
-
-  useEffect(() => {
-    getVaccineInfo();
-  }, []);
+const VaccineInfoPage = async () => {
+  const vaccineList = await getVaccineList();
+  console.log(vaccineList);
 
   return (
     <div>
       접종 정보 페이지
-      <div className="flex flex-wrap gap-3">
-        {vaccineList?.map((item) => {
-          return (
-            <div key={item.id} className="border-[1px] gap-2 w-56 p-3">
-              <p>{item.disease_name}</p>
-              {item.description}
-            </div>
-          );
-        })}
-      </div>
+      <AgeGroup />
+      <AgeFilter />
+      <VaccineList />
     </div>
   );
 };
 
-export default Page;
+export default VaccineInfoPage;
