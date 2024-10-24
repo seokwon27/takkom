59fe162792c224d3b9016c12b526f4f87604eb14
Sehee Jang <seheejang94@gmail.com>
2024-10-24 15:47:05 +0900

    add: 페이지(우리아이페이지, 아이등록하기, 등록하기1단계, 등록하기2단계) 및 컴포넌트(아이등록하기버튼, 우리아이등록하기폼, 우리아이카드)생성

--- src/app/child/page.tsx
+++ src/app/child/page.tsx
@@ -1,5 +1,21 @@
+import ChildCard from "@/_components/child/ChildCard";
+import RegisterButton from "@/_components/child/RegisterButton";
+
+export interface Child {
+  name: string;
+  birthday: string;
+  notes?: string;
+  profileImage?: string;
+}
+
 const page = () => {
-  return <div>아이 카드 페이지</div>;
+  return (
+    <div>
+      <h1>우리 아이 접종</h1>
+      <RegisterButton />
+      <ChildCard child={undefined} />
+    </div>
+  );
 };
 
 export default page;
---
+++ src/app/child/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>아이 등록하기 페이지</div>;
+};
+
+export default page;
