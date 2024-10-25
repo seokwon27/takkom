b2dd3f4b23f8eac9c6ee83dbadfd4d6a4675b6ad
seokwon <tjrtkd95@gmail.com>
2024-10-25 15:06:41 +0900

    update: 백신 데이터 가져오기 및 필터링 함수 생성

--- src/components/vaccineinfo/VaccineList.tsx
+++ src/components/vaccineinfo/VaccineList.tsx
@@ -1,18 +1,29 @@
 "use client";
 
+import { useVaccinationQuery } from "@/query/useVaccinationQuery";
+import { useMultiStore } from "@/utils/globalStore";
+
 const VaccineList = () => {
+  const { data, error, isPending } = useVaccinationQuery();
+  if (isPending) return "접종 정보 로딩중...";
+  if (error) throw new Error(`Error: ${error}`);
+
+  const age = useMultiStore((state) => state.selectedAge);
+
+  const formattedData = data.filter((item) => item.duration === age);
+
   return (
     <div>
       백신 리스트
       <div className="flex flex-wrap gap-3">
-        {/* {vaccineList?.map((item) => {
+        {data?.map((item) => {
           return (
             <div key={item.id} className="border-[1px] gap-2 w-56 p-3">
               <p>{item.disease_name}</p>
               {item.description}
             </div>
           );
-        })} */}
+        })}
       </div>
     </div>
   );
