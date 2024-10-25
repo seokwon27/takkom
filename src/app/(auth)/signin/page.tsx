d94f74abb794401b69b8ad2582f93269322cb30c
이예람 <killhvhl1129@naver.com>
2024-10-25 10:37:17 +0900

    fix : 불필요한 Link 제거

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,9 @@
 import AuthForm from "@/components/auth/AuthForm";
-import Link from "next/link";
 
 export default function page() {
   return (
     <div>
       <AuthForm />
-
-      <div>
-        <Link href={"/signup"}>회원가입</Link>
-      </div>
     </div>
   );
 }
