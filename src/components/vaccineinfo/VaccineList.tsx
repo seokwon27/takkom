be5ea89e9e957a44071cc76cf2e3f3d7a82dd5a4
seokwon <tjrtkd95@gmail.com>
2024-10-26 17:33:03 +0900

    update: 데이터 변경 및 연령별 필터기능 추가

--- src/components/vaccineinfo/VaccineList.tsx
+++ src/components/vaccineinfo/VaccineList.tsx
@@ -1,26 +1,27 @@
 "use client";
 
-import { useVaccinationQuery } from "@/query/useVaccinationQuery";
+import { useVaccineInfoQuery } from "@/query/useVaccineInfoQuery";
 import { useAgeGroupStore } from "@/utils/ageGroupStore";
 
 const VaccineList = () => {
   const { selectedAge } = useAgeGroupStore();
 
-  const { data, error, isPending } = useVaccinationQuery();
+  const { data, error, isPending } = useVaccineInfoQuery();
   if (isPending) return "접종 정보 로딩중...";
   if (error) throw new Error(`Error: ${error}`);
 
-  const formattedData = data.filter((item) => item.duration === selectedAge);
+  const formattedData = data.filter((item) => JSON.parse(item.vaccinate_date || "").includes(selectedAge));
+  // console.log(data);
 
   return (
     <div>
       백신 리스트
       <div className="flex flex-wrap gap-3">
-        {data?.map((item) => {
+        {formattedData?.map((item) => {
           return (
-            <div key={item.id} className="border-[1px] gap-2 w-56 p-3">
+            <div key={item.disease_name} className="border-[1px] gap-2 w-56 p-3">
               <p>{item.disease_name}</p>
-              {item.description}
+              <p className="mt-2">{item.description}</p>
             </div>
           );
         })}
