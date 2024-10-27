178440acc58e176099f41946ec1f2da2a76e76cb
stopbrother <stopbro0323@naver.com>
2024-10-27 12:59:30 +0900

    fix: getVaccinations -> getVaccineRecord 이름변경

--- src/api/vaccineRecord/vaccineRecordApi.ts
+++ src/api/vaccineRecord/vaccineRecordApi.ts
@@ -1,9 +1,9 @@
 import { SupabaseDatabase } from "@/types/supabaseDataType";
 
-export const getVaccinations = async (supabaseClient: SupabaseDatabase, childId: string) => {
+export const getVaccineRecord = async (supabaseClient: SupabaseDatabase, childId: string) => {
   const { data, error } = await supabaseClient
     .from("vaccine_record")
-    .select("id, created_at, child_id, vaccine(id, disease_name, vaccine_turn)")
+    .select("vaccine_id, vaccine(disease_name, vaccine_turn)")
     .eq("child_id", childId);
 
   if (error) throw Error("접종내역 데이터를 가져오는데 실패했습니다.");
