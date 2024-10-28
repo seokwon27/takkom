697577d9092bf4255495c91fe0050a0f4e5426cd
seokwon <tjrtkd95@gmail.com>
2024-10-28 17:13:07 +0900

    style: 모달창 디자인 적용

--- src/components/vaccineinfo/SearchModal.tsx
+++ src/components/vaccineinfo/SearchModal.tsx
@@ -1,26 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import SelectBrtc from "./SelectBrtc";
+import { useState } from "react";
 
 export const SearchModal = () => {
+  const [brtc, setBrtc] = useState();
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button>접종 가능한 병원 찾기</Button>
       </DialogTrigger>
 
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="flex flex-col gap-[48px] w-[252px] h-[192px] py-[28px] px-[40px] rounded-[12px]">
         <DialogHeader>
-          <DialogTitle>지역 선택</DialogTitle>
-          <DialogDescription>{`찾고 싶은 지역을 입력해주세요`}</DialogDescription>
+          <DialogTitle>지역을 선택해 주세요.</DialogTitle>
         </DialogHeader>
 
         <div className="flex">
