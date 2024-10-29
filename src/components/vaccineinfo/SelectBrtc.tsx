ce0e79b95980ab75164807c54e37bcb3b26ae39c
seokwon <tjrtkd95@gmail.com>
2024-10-29 12:51:19 +0900

    fix: 임포트 경로 수정, store 변수명 수정

--- src/components/vaccineinfo/SelectBrtc.tsx
+++ src/components/vaccineinfo/SelectBrtc.tsx
@@ -2,10 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
-import { getBrtcCd, getRegionInfo } from "@/utils/hospital/server-action";
 import { useAgeGroupStore } from "@/utils/zustand/ageGroupStore";
 import { useRouter } from "next/navigation";
-import { Button } from "../ui/button";
+import { getBrtcCd, getRegionInfo } from "@/api/hospitalApi";
 
 type BrtcObj = {
   [key: string]: string;
@@ -52,7 +51,7 @@ const SelectBrtc = () => {
 
   return (
     <>
-      <div className="flex gap-2 mt-6">
+      <div className="">
         <Select
           value={brtc}
           onValueChange={(value) => {
@@ -60,7 +59,7 @@ const SelectBrtc = () => {
             // console.log(value);
           }}
         >
-          <SelectTrigger className="bg-gray-30 border-none ">
+          <SelectTrigger className="">
             <SelectValue placeholder="시/도" />
           </SelectTrigger>
           <SelectContent>
@@ -75,33 +74,43 @@ const SelectBrtc = () => {
             </SelectGroup>
           </SelectContent>
         </Select>{" "}
-        <Select
-          value={sgg}
-          onValueChange={(value) => {
-            setSgg(value);
-            // console.log(value);
-          }}
-        >
-          <SelectTrigger className="bg-gray-30 border-none" disabled={!brtc}>
-            <SelectValue placeholder="시/군/구" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {Object.entries(regionInfo.get(brtc) || {}).map((item) => {
-                return (
-                  <SelectItem value={"" + item[0]} key={item[0]}>
-                    {item[1]}
-                  </SelectItem>
-                );
-              })}
-            </SelectGroup>
-          </SelectContent>
-        </Select>{" "}
       </div>
 
-      <Button onClick={handleClick} disabled={!brtc || !sgg} className="mt-9">
+      <div>
+        <div className="">
+          <Select
+            value={sgg}
+            onValueChange={(value) => {
+              setSgg(value);
+              // console.log(value);
+            }}
+          >
+            <SelectTrigger className="" disabled={brtc === ""}>
+              <SelectValue placeholder="시/군/구" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {Object.entries(regionInfo.get(brtc) || {}).map((item) => {
+                  return (
+                    <SelectItem value={"" + item[0]} key={item[0]}>
+                      {item[1]}
+                    </SelectItem>
+                  );
+                })}
+              </SelectGroup>
+            </SelectContent>
+          </Select>{" "}
+        </div>
+      </div>
+
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+        disabled={brtc === "" || sgg === ""}
+      >
         확인
-      </Button>
+      </button>
     </>
   );
 };
