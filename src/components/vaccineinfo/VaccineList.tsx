4569585400b2298deecfe45ddafbf79ec3e74cc5
seokwon <tjrtkd95@gmail.com>
2024-10-30 12:57:30 +0900

    test: 수파베이스 데이터 테스트

--- src/components/vaccineinfo/VaccineList.tsx
+++ src/components/vaccineinfo/VaccineList.tsx
@@ -7,6 +7,7 @@ const VaccineList = () => {
   const { selectedAge, setCurrentDisease, currentDisease } = useAgeGroupStore();
 
   const { data, error, isPending } = useVaccineInfoQuery();
+  // const {data, error, isPending} = useVaccinationQuery()
   if (isPending) return "접종 정보 로딩중...";
   if (error) throw new Error(`Error: ${error}`);
 
@@ -20,13 +21,14 @@ const VaccineList = () => {
         {formattedData?.map((item) => {
           return (
             <div
-              key={item.disease_name}
+              key={`${item.vaccine_name} ${item.vaccine_turn}`}
               className={`${currentDisease === item.disease_name ? "border-black" : ""} border-[2px] gap-2 w-56 p-3`}
               onClick={() => {
-                setCurrentDisease(item.disease_name);
+                setCurrentDisease(item.disease_name || "");
               }}
             >
               <p>{item.disease_name}</p>
+              <p>{`${item.vaccine_name} ${item.vaccine_turn}차`}</p>
               <p className="mt-2">{item.description}</p>
             </div>
           );
