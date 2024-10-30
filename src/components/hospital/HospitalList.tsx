16f3146b9cb43118b48923282ffca12a92934f2a
joycie416 <joycie416@gmail.com>
2024-10-30 15:13:44 +0900

    refactor: TanStack Query 적용

--- src/components/hospital/HospitalList.tsx
+++ src/components/hospital/HospitalList.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import HospitalCard from "./HospitalCard";
 import HospitalPagination from "./HospitalPagination";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 const HospitalList = () => {
   const searchParams = useSearchParams();
@@ -17,18 +18,31 @@ const HospitalList = () => {
     Number(searchParams.get("pageNo")) ?? 1
   ];
   const defaultData: HospitalData = { items: [], totalCount: 0, maxPage: 1 };
-  const [hospitalData, setHospitalData] = useState<HospitalData>(defaultData);
+  // const [hospitalData, setHospitalData] = useState<HospitalData>(defaultData);
 
-  useEffect(() => {
-    const fetchHospitalsMultiConditions = async (input: HospitalsMutliConditionParams): Promise<void> => {
-      const data = await getHospitalsMutliConditions(input);
-      console.log("hospitals :", data);
-      setHospitalData(data as HospitalData);
-    };
-    if (brtcCd && sggCd) {
-      fetchHospitalsMultiConditions({ brtcCd, sggCd, addr, org, disease });
-    }
-  }, [brtcCd, sggCd, addr, org, disease]);
+  // useEffect(() => {
+  //   const fetchHospitalsMultiConditions = async (input: HospitalsMutliConditionParams): Promise<void> => {
+  //     const data = await getHospitalsMutliConditions(input);
+  //     console.log("hospitals :", data);
+  //     setHospitalData(data as HospitalData);
+  //   };
+  //   if (brtcCd && sggCd) {
+  //     fetchHospitalsMultiConditions({ brtcCd, sggCd, addr, org, disease });
+  //   }
+  // }, [brtcCd, sggCd, addr, org, disease]);
+
+  const {data: hospitalData, isLoading, isError, isFetching, error} = useQuery({
+    queryKey: ['hospital', {brtcCd}, {sggCd}, {addr}, org && {org}, disease && {disease}],
+    queryFn: () => getHospitalsMutliConditions({ brtcCd, sggCd, addr, org, disease }),
+    staleTime:Infinity
+  })
+
+  if (isLoading || isFetching) {
+    return <div>데이터를 불러오는 중입니다. 잠시만 기다려주세요.</div>
+  }
+  if (isError || !hospitalData) {
+    return <div>에러 : {!hospitalData ? '에러가 발생했습니다.' : error?.message}</div>
+  }
 
   return (
     <div className="w-full flex flex-col">
