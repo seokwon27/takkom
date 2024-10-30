0c7119b4734ce1b7722d5161ba642afff62a9c5d
seokwon <tjrtkd95@gmail.com>
2024-10-30 19:16:54 +0900

    update: 데이터 타입 최신화

--- database.types.ts
+++ database.types.ts
@@ -1,285 +1,303 @@
-export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
   public: {
     Tables: {
       child: {
         Row: {
-          birth: string | null;
-          created_at: string;
-          id: string;
-          name: string | null;
-          notes: string | null;
-          profile: string | null;
-          updated_at: string | null;
-          user_id: string | null;
-        };
+          birth: string | null
+          created_at: string
+          id: string
+          name: string | null
+          notes: string | null
+          profile: string | null
+          updated_at: string | null
+          user_id: string | null
+        }
         Insert: {
-          birth?: string | null;
-          created_at?: string;
-          id?: string;
-          name?: string | null;
-          notes?: string | null;
-          profile?: string | null;
-          updated_at?: string | null;
-          user_id?: string | null;
-        };
+          birth?: string | null
+          created_at?: string
+          id?: string
+          name?: string | null
+          notes?: string | null
+          profile?: string | null
+          updated_at?: string | null
+          user_id?: string | null
+        }
         Update: {
-          birth?: string | null;
-          created_at?: string;
-          id?: string;
-          name?: string | null;
-          notes?: string | null;
-          profile?: string | null;
-          updated_at?: string | null;
-          user_id?: string | null;
-        };
-        Relationships: [];
-      };
+          birth?: string | null
+          created_at?: string
+          id?: string
+          name?: string | null
+          notes?: string | null
+          profile?: string | null
+          updated_at?: string | null
+          user_id?: string | null
+        }
+        Relationships: []
+      }
       user: {
         Row: {
-          created_at: string;
-          email: string | null;
-          id: string;
-          name: string | null;
-          updated_at: string | null;
-          user_id: string | null;
-        };
+          created_at: string
+          email: string | null
+          id: string
+          name: string | null
+          updated_at: string | null
+          user_id: string | null
+        }
         Insert: {
-          created_at?: string;
-          email?: string | null;
-          id?: string;
-          name?: string | null;
-          updated_at?: string | null;
-          user_id?: string | null;
-        };
+          created_at?: string
+          email?: string | null
+          id?: string
+          name?: string | null
+          updated_at?: string | null
+          user_id?: string | null
+        }
         Update: {
-          created_at?: string;
-          email?: string | null;
-          id?: string;
-          name?: string | null;
-          updated_at?: string | null;
-          user_id?: string | null;
-        };
-        Relationships: [];
-      };
+          created_at?: string
+          email?: string | null
+          id?: string
+          name?: string | null
+          updated_at?: string | null
+          user_id?: string | null
+        }
+        Relationships: []
+      }
       vaccine: {
         Row: {
-          additional: boolean;
-          description: string | null;
-          disease_name: string | null;
-          duration: string | null;
-          id: string;
-          vaccinate_date: number | null;
-          vaccine_name: string | null;
-          vaccine_turn: number | null;
-        };
+          additional: boolean
+          description: string | null
+          disease_name: string | null
+          duration: string | null
+          id: string
+          vaccinate_date: number | null
+          vaccine_name: string | null
+          vaccine_turn: number | null
+        }
         Insert: {
-          additional?: boolean;
-          description?: string | null;
-          disease_name?: string | null;
-          duration?: string | null;
-          id: string;
-          vaccinate_date?: number | null;
-          vaccine_name?: string | null;
-          vaccine_turn?: number | null;
-        };
+          additional?: boolean
+          description?: string | null
+          disease_name?: string | null
+          duration?: string | null
+          id: string
+          vaccinate_date?: number | null
+          vaccine_name?: string | null
+          vaccine_turn?: number | null
+        }
         Update: {
-          additional?: boolean;
-          description?: string | null;
-          disease_name?: string | null;
-          duration?: string | null;
-          id?: string;
-          vaccinate_date?: number | null;
-          vaccine_name?: string | null;
-          vaccine_turn?: number | null;
-        };
-        Relationships: [];
-      };
+          additional?: boolean
+          description?: string | null
+          disease_name?: string | null
+          duration?: string | null
+          id?: string
+          vaccinate_date?: number | null
+          vaccine_name?: string | null
+          vaccine_turn?: number | null
+        }
+        Relationships: []
+      }
       vaccine_duplicate: {
         Row: {
-          additional: boolean;
-          description: string | null;
-          disease_name: string | null;
-          duration: string | null;
-          id: string;
-          vaccinate_date: string | null;
-          vaccine_name: string | null;
-          vaccine_turn: number | null;
-        };
+          description: string | null
+          disease_name: string | null
+          id: string
+          process: string
+          target: string | null
+          vaccinate_date: string | null
+          vaccine_name: string | null
+          vaccine_turn: string | null
+        }
         Insert: {
-          additional?: boolean;
-          description?: string | null;
-          disease_name?: string | null;
-          duration?: string | null;
-          id: string;
-          vaccinate_date?: string | null;
-          vaccine_name?: string | null;
-          vaccine_turn?: number | null;
-        };
+          description?: string | null
+          disease_name?: string | null
+          id: string
+          process: string
+          target?: string | null
+          vaccinate_date?: string | null
+          vaccine_name?: string | null
+          vaccine_turn?: string | null
+        }
         Update: {
-          additional?: boolean;
-          description?: string | null;
-          disease_name?: string | null;
-          duration?: string | null;
-          id?: string;
-          vaccinate_date?: string | null;
-          vaccine_name?: string | null;
-          vaccine_turn?: number | null;
-        };
-        Relationships: [];
-      };
+          description?: string | null
+          disease_name?: string | null
+          id?: string
+          process?: string
+          target?: string | null
+          vaccinate_date?: string | null
+          vaccine_name?: string | null
+          vaccine_turn?: string | null
+        }
+        Relationships: []
+      }
       vaccine_record: {
         Row: {
-          child_id: string | null;
-          created_at: string;
-          id: string;
-          vaccine_id: string | null;
-        };
+          child_id: string
+          created_at: string
+          id: string
+          vaccine_id: string
+        }
         Insert: {
-          child_id?: string | null;
-          created_at?: string;
-          id?: string;
-          vaccine_id?: string | null;
-        };
+          child_id?: string
+          created_at?: string
+          id?: string
+          vaccine_id: string
+        }
         Update: {
-          child_id?: string | null;
-          created_at?: string;
-          id?: string;
-          vaccine_id?: string | null;
-        };
+          child_id?: string
+          created_at?: string
+          id?: string
+          vaccine_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "vaccine_record_child_id_fkey";
-            columns: ["child_id"];
-            isOneToOne: false;
-            referencedRelation: "child";
-            referencedColumns: ["id"];
+            foreignKeyName: "vaccine_record_child_id_fkey"
+            columns: ["child_id"]
+            isOneToOne: false
+            referencedRelation: "child"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "vaccine_record_vaccine_id_fkey";
-            columns: ["vaccine_id"];
-            isOneToOne: false;
-            referencedRelation: "vaccine";
-            referencedColumns: ["id"];
-          }
-        ];
-      };
+            foreignKeyName: "vaccine_record_vaccine_id_fkey"
+            columns: ["vaccine_id"]
+            isOneToOne: false
+            referencedRelation: "vaccine"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       vaccineinfo: {
         Row: {
-          description: string;
-          disease_name: string;
-          vaccinate_date: string | null;
-        };
+          description: string
+          disease_name: string
+          vaccinate_date: string | null
+        }
         Insert: {
-          description: string;
-          disease_name: string;
-          vaccinate_date?: string | null;
-        };
+          description: string
+          disease_name: string
+          vaccinate_date?: string | null
+        }
         Update: {
-          description?: string;
-          disease_name?: string;
-          vaccinate_date?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          description?: string
+          disease_name?: string
+          vaccinate_date?: string | null
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type PublicSchema = Database[Extract<keyof Database, "public">];
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-  PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"]) | { schema: keyof Database },
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-  ? (PublicSchema["Tables"] & PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R;
-    }
-    ? R
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I;
-    }
-    ? I
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U;
-    }
-    ? U
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends keyof PublicSchema["Enums"] | { schema: keyof Database },
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-  : never;
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
 
 export type CompositeTypes<
-  PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"] | { schema: keyof Database },
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof Database
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never
+    : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-  ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-  : never;
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
