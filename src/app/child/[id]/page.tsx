cb12002d9e243dcefc8a8229424f918a8a6604f5
stopbrother <stopbro0323@naver.com>
2024-10-30 10:36:00 +0900

    update: 수정하기 링크 추가 및 vaccine_record 데이터 prefetching

--- src/app/child/[id]/page.tsx
+++ src/app/child/[id]/page.tsx
@@ -1,18 +1,22 @@
 import { getVaccines } from "@/api/vaccineApi";
+import { getVaccineRecord } from "@/api/vaccineRecord/vaccineRecordApi";
+import { Button } from "@/components/ui/button";
+import VaccineRecord from "@/components/vaccinerecord/VaccineRecord";
 
-import VaccineRecord from "@/components/vaccinerecord/VaccinateRecord";
 import { createClient } from "@/utils/supabase/server";
 import { groupVaccines } from "@/utils/vaccineRecord/vaccinesRecord";
 
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import Link from "next/link";
 
-// interface VaccinatePageProps {
-//   params: {
-//     id: string;
-//   };
-// }
+interface VaccinatePageProps {
+  params: {
+    id: string;
+  };
+}
 
-const VaccinatePage = async () => {
+const VaccinatePage = async ({ params }: VaccinatePageProps) => {
+  const childId = params.id;
   const serverClient = createClient();
   const queryClient = new QueryClient();
 
@@ -24,9 +28,25 @@ const VaccinatePage = async () => {
     }
   });
 
+  await queryClient.prefetchQuery({
+    queryKey: ["vaccine_record", childId],
+    queryFn: () => getVaccineRecord(serverClient, childId)
+  });
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl">우리 아이 접종 내역</h1>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-2">
+            <div>전체</div>
+            <div>접종 완료</div>
+            <div>미접종</div>
+          </div>
+          <Link href={`/child/${params.id}/edit`}>
+            <Button>수정하기</Button>
+          </Link>
+        </div>
         <VaccineRecord />
       </div>
     </HydrationBoundary>
