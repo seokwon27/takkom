ca2d0d283ea9a987040db008b061fc1f2dca247b
joycie416 <joycie416@gmail.com>
2024-10-30 19:02:01 +0900

    update: 주석 삭제

--- src/components/hospital/InfoTag.tsx
+++ src/components/hospital/InfoTag.tsx
@@ -1,17 +1,3 @@
-// import React from "react";
-
-// const InfoTag = ({ isVisible }: { isVisible: boolean }) => {
-//   return (
-//     <div className={`${isVisible ? "animate-fadein bg-gray-500 text-white" : "animate-fadeout text-transparent"} p-2 rounded-t-lg rounded-r-lg`}>
-//     {/* <div className={`${isVisible ? null : "hidden"} p-2 rounded-t-lg rounded-r-lg`}> */}
-//       <p>도로명 주소를 입력해 주세요.</p>
-//       <p>ex. 서울특별시 관악구 <span className={`${isVisible ? "animate-fadein text-primary-500 bg-transparent" : "animate-fadeout"} bg-transparent`}>신림로</span></p>
-//     </div>
-//   );
-// };
-
-// export default InfoTag;
-
 import React from "react";
 
 const InfoTag = ({ isVisible }: { isVisible: boolean }) => {
