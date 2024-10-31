467e208aecf0827cff2f48f0e4e1789539fb0a21
Sehee Jang <seheejang94@gmail.com>
2024-10-31 16:18:34 +0900

    update: 우리아이 정보 tanstack query 적용

--- src/api/childInfoApi.ts
+++ src/api/childInfoApi.ts
@@ -1,17 +1,9 @@
-import { Child } from "@/types/childType";
 import { SupabaseDatabase } from "@/types/supabaseDataType";
 
-export const getChildInfo = async (
-  client: SupabaseDatabase,
-  userId: string
-) => {
-  const { data, error } = await client
-    .from("child")
-    .select("*")
-    .eq("user_id", userId)
-    .single();
+export const getChildInfo = async (client: SupabaseDatabase, userId: string) => {
+  const { data, error } = await client.from("child").select("*").eq("user_id", userId);
 
   if (error) throw new Error(error.message);
 
-  return data; 
+  return data;
 };
--- src/app/child/page.tsx
+++ src/app/child/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ChildCard from "@/components/child/ChildCard";
 import RegisterButton from "@/components/child/RegisterButton";
 import { useChildInfoQuery } from "@/query/useChildQuery";
@@ -5,7 +6,7 @@ import { useChildInfoQuery } from "@/query/useChildQuery";
 const ChildPage = () => {
   const userId = "4c656382-4114-4929-ab84-89ec5a6ddef9"; // 테스트 userId
 
-  const { data: child, isLoading, error } = useChildInfoQuery(userId);
+  const { data: children, isLoading, error } = useChildInfoQuery(userId);
 
   if (isLoading) return <p>로딩 중...</p>;
   if (error) return <p>오류가 발생했습니다: {error.message}</p>;
@@ -14,7 +15,13 @@ const ChildPage = () => {
     <div>
       <h1>우리 아이 접종</h1>
       <RegisterButton />
-      {child && child.id ? <ChildCard child={child} /> : <p>등록된 아이가 없습니다.</p>}
+      {children && children.length > 0 ? ( // children 배열을 확인
+        children.map((child) => (
+          <ChildCard key={child.id} child={child} /> // 각 child 정보를 ChildCard로 전달
+        ))
+      ) : (
+        <p>등록된 아이가 없습니다.</p>
+      )}
     </div>
   );
 };
