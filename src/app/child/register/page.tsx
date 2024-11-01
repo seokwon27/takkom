5fd0d8db6c462cd2a4bf973870249584fc666b2a
joycie416 <joycie416@gmail.com>
2024-11-01 14:15:21 +0900

    fix: merge conflict 수정

--- src/app/child/register/page.tsx
+++ src/app/child/register/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import { useUserQuery } from "@/api/userApi";
 import RegisterForm from "@/components/child/RegisterForm";
 import browserClient from "@/utils/supabase/client";
 
 const RegisterPage = () => {
   // useUserQuery를 이용하여 현재 로그인한 사용자 정보를 가져옵니다.
-  const { user, isUserLoading, isUserError } = useUserQuery(browserClient);
+  const { data: user, isLoading: isUserLoading, isError: isUserError } = useUserQuery(browserClient);
 
   // 사용자 정보를 로드하는 동안 로딩 표시
   if (isUserLoading) return <p>로딩 중...</p>;
