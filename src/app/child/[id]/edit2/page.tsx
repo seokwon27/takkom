d8cd88ef18ad7de93bf5d5087a34d77bb5337936
stopbrother <stopbro0323@naver.com>
2024-11-05 19:08:44 +0900

    update: image추가 및 css적용

--- src/app/child/[id]/edit2/page.tsx
+++ src/app/child/[id]/edit2/page.tsx
@@ -6,7 +6,10 @@ import CheckboxFormWrap from "@/components/vaccinerecord/CheckboxFormWrap";
 import { createClient } from "@/utils/supabase/server";
 import { groupVaccines } from "@/utils/vaccineRecord/vaccinesRecord";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
-import React from "react";
+
+import Image from "next/image";
+import Vaccination11 from "../../../../assets/images/vaccinerecord/vaccination-1-1.png";
+import Vaccination12 from "../../../../assets/images/vaccinerecord/vaccination-1-2.png";
 
 interface VaccineRecordEditPageProps {
   params: {
@@ -37,10 +40,19 @@ const VaccineRecordEditPage = async ({ params }: VaccineRecordEditPageProps) => 
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl">우리 아이 접종 내역</h1>
-        <div className="flex flex-row justify-between"></div>
-        <CheckboxFormWrap childId={params.id} />
+      <div className="flex flex-col gap-10 items-start relative">
+        <div className="inline-flex flex-col items-start gap-12">
+          <Image src={Vaccination11} alt="Vaccination" className="w-20 h-20 object-cover relative" />
+          <div className="inline-flex flex-col items-start gap-3 relative">
+            <h1 className="self-stretch mt-[-1.00px] font-heading-XXL text-gray-900">우리 아이 접종 내역</h1>
+          </div>
+          <Image
+            src={Vaccination12}
+            alt="Vaccination"
+            className="absolute w-[100px] h-[100px] -top-0.5 -left-px object-cover"
+          />
+          <CheckboxFormWrap childId={params.id} />
+        </div>
       </div>
     </HydrationBoundary>
   );
