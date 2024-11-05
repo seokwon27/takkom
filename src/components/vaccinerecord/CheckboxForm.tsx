7a8deadcb56f89012462bd60d744eea36350ad42
stopbrother <stopbro0323@naver.com>
2024-11-05 22:22:05 +0900

    update: 수정페이지에 탭 추가

--- src/components/vaccinerecord/CheckboxForm.tsx
+++ src/components/vaccinerecord/CheckboxForm.tsx
@@ -7,6 +7,7 @@ import { useAddVaccineRecordMutation, useDeleteVaccineRecordMutation } from "@/q
 import { useVaccineQuery, useVaccineRecordQuery } from "@/query/useVaccineRecordQuery";
 import { ReactNode } from "react";
 import VaccineRecordList from "./VaccineRecordList";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 interface CheckboxFormProps {
   childId: string;
@@ -47,10 +48,80 @@ const CheckboxForm = ({ childId, onSuccess, children }: CheckboxFormProps) => {
     onSuccess();
   };
 
+  const getFilteredVaccineData = (filter: "전체" | "접종 완료" | "미접종") => {
+    if (filter === "전체") return vaccineData;
+
+    return vaccineData?.filter((vaccine) => {
+      const allCheckedVaccine = vaccine.disease.every((disease) => disease.ids.every((id) => vaccinated.has(id)));
+
+      if (filter === "접종 완료") return allCheckedVaccine;
+
+      if (filter === "미접종") return !allCheckedVaccine;
+    });
+  };
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <VaccineRecordList data={vaccineData} vaccinated={vaccinated} edit={true} control={form.control} />
+        <Tabs
+          defaultValue="전체"
+          className="flex flex-col items-start gap-4 self-stretch relative w-full flex-[0_0_auto]"
+        >
+          <TabsList className="flex justify-between items-center px-8 py-0 self-stretch w-full bg-transparent">
+            <div className="inline-flex items-center gap-2 relative ">
+              <TabsTrigger
+                value="전체"
+                className="group flex w-20 items-center justify-center gap-2.5 p-2 relative rounded-none data-[state=active]:border-b-2 data-[state=active]:border-gray-700"
+              >
+                <p className="relative w-fit mt-[-2.00px] text-gray-300 group-data-[state=active]:text-gray-700 whitespace-nowrap hover:text-gray-700">
+                  전체
+                </p>
+              </TabsTrigger>
+              <TabsTrigger
+                value="접종 완료"
+                className="group flex w-20 items-center justify-center gap-2.5 p-2 relative rounded-none data-[state=active]:border-b-2 data-[state=active]:border-gray-700 data-[state=active]:text-gray-700"
+              >
+                <p className="relative w-fit mt-[-2.00px] text-gray-300 group-data-[state=active]:text-gray-700 whitespace-nowrap hover:text-gray-700">
+                  접종완료
+                </p>
+              </TabsTrigger>
+              <TabsTrigger
+                value="미접종"
+                className="group flex w-20 items-center justify-center gap-2.5 p-2 relative rounded-none data-[state=active]:border-b-2 data-[state=active]:border-gray-700"
+              >
+                <p className="relative w-fit mt-[-2.00px] text-gray-300 group-data-[state=active]:text-gray-700 whitespace-nowrap hover:text-gray-700">
+                  미접종
+                </p>
+              </TabsTrigger>
+            </div>
+          </TabsList>
+
+          <TabsContent value="전체" className="w-full">
+            <VaccineRecordList
+              data={getFilteredVaccineData("전체")}
+              vaccinated={vaccinated}
+              edit={true}
+              control={form.control}
+            />
+          </TabsContent>
+          <TabsContent value="접종 완료" className="w-full">
+            <VaccineRecordList
+              data={getFilteredVaccineData("접종 완료")}
+              vaccinated={vaccinated}
+              edit={true}
+              control={form.control}
+            />
+          </TabsContent>
+          <TabsContent value="미접종" className="w-full">
+            <VaccineRecordList
+              data={getFilteredVaccineData("미접종")}
+              vaccinated={vaccinated}
+              edit={true}
+              control={form.control}
+            />
+          </TabsContent>
+        </Tabs>
+
         {children}
         <FormMessage />
       </form>
