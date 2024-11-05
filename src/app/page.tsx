0b78cd3636b3da6b34ca0a2a676e0ecc399d0fb8
seokwon <tjrtkd95@gmail.com>
2024-11-05 22:10:58 +0900

    fix: 폴더명 오타 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ const HomePage = () => {
       <div className="flex gap-6 mt-[77px] w-full max-w-[1200px] m-auto">
         <Link
           href={"/vaccineinfo"}
-          className="flex bg-[url('/hompage/vaccineInfoBg.svg')] px-12 py-20 rounded-3xl text-white w-[736px] h-[472px] relative"
+          className="flex bg-[url('/homepage/vaccineInfoBg.svg')] px-12 py-20 rounded-3xl text-white w-[736px] h-[472px] relative"
         >
-          <img src="/hompage/linkPerson.svg" className="absolute -bottom-12 right-11" />
+          <img src="/homepage/linkPerson.svg" className="absolute -bottom-12 right-11" />
           <div className="flex flex-col justify-center gap-6">
             <div className="z-10">
               <p className="text-[32px] font-bold">지금 아이에게 필요한</p>
@@ -17,7 +17,7 @@ const HomePage = () => {
             </div>
             <div className="flex justify-center gap-[6px] bg-white text-primary-400 max-w-[135px] p-2 rounded-[50px] text-xl font-bold">
               <div>
-                <img src="/hompage/SearchIcon.svg" />
+                <img src="/homepage/SearchIcon.svg" />
               </div>
               <p>보러가기</p>
             </div>
@@ -26,7 +26,7 @@ const HomePage = () => {
 
         <div className="flex flex-col gap-6 ">
           <Link className="p-[40px] rounded-3xl w-[440px] h-[224px] bg-[#F0F9FF] relative" href={"/child"}>
-            <img src="/hompage/childLinkIcon.svg" className="absolute right-0" />
+            <img src="/homepage/childLinkIcon.svg" className="absolute right-0" />
             <div className="flex flex-col gap-3">
               <p className="text-2xl text-gray-800 font-bold "> 우리 아이 접종 관리하기</p>
               <p className="text-gray-600">
@@ -34,13 +34,13 @@ const HomePage = () => {
                 접종 내역을 체크할 수 있어요!
               </p>
               <div className="flex gap-2 justify-center items-center border-[1px] border-primary-300 rounded-[19px] w-[103px] h-[38px]">
-                <img src="/hompage/addCircle.svg" />
+                <img src="/homepage/addCircle.svg" />
                 <p className="text-primary-300 font-semibold">등록하기</p>
               </div>
             </div>
           </Link>
           <Link href={"/search"} className="">
-            <img src="/hompage/hospitalLink.svg" />
+            <img src="/homepage/hospitalLink.svg" />
           </Link>
         </div>
       </div>
