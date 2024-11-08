7f6f0b4b1ef454666c35b5d37f435b66e9130596
stopbrother <stopbro0323@naver.com>
2024-11-08 17:55:07 +0900

    refactor: 수정페이지에서도 탭 구분하기

--- src/components/vaccinerecord/VaccineRecordTabs.tsx
+++ src/components/vaccinerecord/VaccineRecordTabs.tsx
@@ -5,12 +5,16 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import VaccineRecordList from "./VaccineRecordList";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { Control } from "react-hook-form";
+import { FormValues } from "./CheckboxForm";
 
 interface VaccineRecordProps {
   childId: string;
+  edit: boolean;
+  control?: Control<FormValues>;
 }
 
-const VaccineRecord = ({ childId }: VaccineRecordProps) => {
+const VaccineRecordTabs = ({ childId, edit, control }: VaccineRecordProps) => {
   const { data: vaccineData, isLoading: vaccineLoading } = useVaccineQuery();
   const { data: vaccineRecord, isLoading: recordLoading } = useVaccineRecordQuery(childId);
 
@@ -34,8 +38,8 @@ const VaccineRecord = ({ childId }: VaccineRecordProps) => {
 
   return (
     <Tabs defaultValue="전체" className="flex flex-col items-start gap-4 self-stretch relative w-full flex-[0_0_auto]">
-      <TabsList className="flex justify-between items-center px-8 py-0 self-stretch w-full bg-transparent">
-        <div className="inline-flex items-center gap-2 relative ">
+      <div className="flex justify-between items-center px-8 py-0 self-stretch w-full">
+        <TabsList className="inline-flex items-center gap-2 relative bg-transparent">
           {tabs.map((tab) => (
             <TabsTrigger
               key={tab}
@@ -47,24 +51,33 @@ const VaccineRecord = ({ childId }: VaccineRecordProps) => {
               </p>
             </TabsTrigger>
           ))}
-        </div>
-        <Link
-          href={`/child/${childId}/edit2`}
-          className="inline-flex justify-center gap-2.5 px-3 py-1.5 rounded-[15px] items-center"
-        >
-          <Button className="w-fit mt-[-1.00px] text-primary-300 whitespace-nowrap bg-transparent hover:bg-primary-50">
-            수정하기
-          </Button>
-        </Link>
-      </TabsList>
+        </TabsList>
+
+        {/* view페이지에서만 버튼 on */}
+        {!edit && (
+          <Link
+            href={`/child/${childId}/edit2`}
+            className="inline-flex justify-center gap-2.5 px-3 py-1.5 rounded-[15px] items-center"
+          >
+            <Button className="w-fit mt-[-1.00px] text-primary-300 whitespace-nowrap bg-transparent hover:bg-primary-50">
+              수정하기
+            </Button>
+          </Link>
+        )}
+      </div>
 
       {tabs.map((tab) => (
         <TabsContent key={tab} value={tab} className="w-full">
-          <VaccineRecordList data={getFilteredVaccineData(tab)} vaccinated={vaccinated} edit={false} />
+          <VaccineRecordList
+            data={getFilteredVaccineData(tab)}
+            vaccinated={vaccinated}
+            edit={edit}
+            control={edit ? control : undefined}
+          />
         </TabsContent>
       ))}
     </Tabs>
   );
 };
 
-export default VaccineRecord;
+export default VaccineRecordTabs;
