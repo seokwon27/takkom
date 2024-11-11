bbc491fabe28ba0c7998c3ece9a22b181025771c
seokwon <tjrtkd95@gmail.com>
2024-11-11 20:20:06 +0900

    test: 반응형 페이지이름 출력 테스트

--- src/app/vaccineinfo/page.tsx
+++ src/app/vaccineinfo/page.tsx
@@ -4,7 +4,13 @@ import VaccineList from "@/components/vaccineinfo/VaccineList";
 
 const VaccineInfoPage = async () => {
   return (
-    <div className="container flex flex-col max-w-[800px] mx-auto mt-[64px] justify-center">
+    <div
+      className="container flex flex-col mx-auto max-w-[327px] justify-center
+    sm:max-w-[800px] sm:mt-[64px]"
+    >
+      <div className="py-[6px] mb-4 sm:hidden">
+        <p className="text-gray-800 text-xl font-semibold">예방접종</p>
+      </div>
       <AgeGroup />
       <AgeFilter />
       <VaccineList />
