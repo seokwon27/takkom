603d06db6d318f6b4ad874f7ccbabee9278c600c
seokwon <tjrtkd95@gmail.com>
2024-11-11 20:20:59 +0900

    fix: svg파일 변수 파스칼케이스 변경, 이미지태그 반응형 크기 설정

--- src/components/vaccineinfo/AgeGroup.tsx
+++ src/components/vaccineinfo/AgeGroup.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { useAgeGroupStore } from "@/store/ageGroupStore";
-import category from "../../../public/ageGroup/category.svg";
-import newborn from "../../../public/ageGroup/newborn-baby.svg";
-import yeonga from "../../../public/ageGroup/yeong-a.svg";
-import yua from "../../../public/ageGroup/yua.svg";
-import children from "../../../public/ageGroup/children.svg";
+import Category from "../../../public/ageGroup/category.svg";
+import Newborn from "../../../public/ageGroup/newborn-baby.svg";
+import Yeonga from "../../../public/ageGroup/yeong-a.svg";
+import Yua from "../../../public/ageGroup/yua.svg";
+import Children from "../../../public/ageGroup/children.svg";
 import Image from "next/image";
 
 const AgeGroup = () => {
   return (
     <div className="text-center items-center justify-center">
-      <div className="flex text-center justify-between p-2 w-full">
+      <div className="flex text-center justify-between w-full">
         <AgeCard age={"전체"} />
         <AgeCard age={"신생아"} />
         <AgeCard age={"영아기"} />
@@ -26,18 +26,18 @@ const AgeCard = ({ age }: { age: string }) => {
   const division = CategorySvg[age];
   return (
     <div className="flex flex-col gap-3 hover:cursor-pointer text-lg font-semibold" onClick={() => setAgeGroup(age)}>
-      <Image src={division} alt={`${age}`} width={100} height={100} />
+      <Image src={division} alt={`${age}`} className="w-[54px] h-[54px] sm:w-[100px] sm:h-[100px]" />
       <p className={`${age === ageGroup ? "text-[#303030]" : "text-[#B0B0B0]"}  text-lg font-semibold`}>{age}</p>
     </div>
   );
 };
 
 const CategorySvg: { [key: string]: string } = {
-  전체: category,
-  신생아: newborn,
-  영아기: yeonga,
-  유아기: yua,
-  아동기: children
+  전체: Category,
+  신생아: Newborn,
+  영아기: Yeonga,
+  유아기: Yua,
+  아동기: Children
 };
 
 export default AgeGroup;
