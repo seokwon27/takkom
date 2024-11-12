10a90434947e6388b74235a2486808c2dd514e7d
seokwon <tjrtkd95@gmail.com>
2024-11-12 17:24:40 +0900

    style: 백신카드 반응형 디자인 적용(접종태그 미적용)

--- src/components/vaccineinfo/vaccineCard.tsx
+++ src/components/vaccineinfo/vaccineCard.tsx
@@ -1,7 +1,9 @@
 import { useAgeGroupStore } from "@/store/ageGroupStore";
 import React from "react";
-import FreeTag from "../../../public/common/free-tag.svg";
 import Image from "next/image";
+import Target from "../../../public/vaccineInfo/target.svg";
+import Process from "../../../public/vaccineInfo/process.svg";
+
 const VaccineCard = ({
   disease,
   vaccine,
@@ -19,30 +21,39 @@ const VaccineCard = ({
     <div
       className={`${
         vaccineId === vaccine ? "border-primary-400 border-2" : ""
-      }  px-14 py-16 flex flex-col gap-8 rounded-3xl max-h-[358px] hover:cursor-pointer shadow-[0px_0px_16px_0px_rgba(114,114,114,0.10)]`}
+      }  p-10 flex flex-col gap-8 rounded-3xl max-h-[358px] hover:cursor-pointer shadow-[0px_0px_16px_0px_rgba(114,114,114,0.10)]
+      max-sm:max-h-[140px]  max-sm:px-6 max-sm:py-4 max-sm:gap-[10px]
+      `}
       onClick={() => {
+        if (vaccineId === vaccine) {
+          setCurrentDisease("");
+          setVaccineId("");
+          return;
+        }
         setCurrentDisease(disease ?? "");
         setVaccineId(vaccine);
       }}
     >
-      <div className="flex flex-col gap-10">
-        <div className="flex flex-col gap-2">
-          <p className="text-2xl font-bold">{disease}</p>
-          <p className="text-base font-normal text-gray-600">{vaccine}</p>
+      <div className="flex flex-col gap-10 max-sm:gap-[10px]">
+        <div className="flex flex-col gap-2 max-sm:flex-row max-sm:gap-1 max-sm:items-end">
+          <p className="text-heading-xl font-bold max-sm:text-heading-xs break-keep">{disease}</p>
+          <p className="text-label-xl font-normal text-gray-600 max-sm:text-label-s">{vaccine}</p>
         </div>
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-3">
-            <p className="text-base font-semibold text-gray-800">{`대상`}</p>
-            <p className="text-base font-normal text-gray-600">{target}</p>
+        <div className="flex flex-col gap-2 max-sm:gap-1">
+          <div className="flex gap-3 max-sm:gap-1">
+            <Image src={Target} alt="대상" />
+            <p className="text-title-s font-semibold text-gray-800 max-sm:text-label-m">{`대상`}</p>
+            <p className="text-label-xl font-normal text-gray-600 max-sm:text-text-m">{target}</p>
           </div>
-          <div className="flex gap-3">
-            <p className="text-base font-semibold text-gray-800">{`방법`}</p>
-            <p className="text-base font-normal text-gray-600">{process}</p>
+          <div className="flex gap-3 max-sm:gap-1">
+            <Image src={Process} alt="방법" />
+            <p className="text-title-s font-semibold text-gray-800 max-sm:text-label-m ">{`방법`}</p>
+            <p className="text-label-xl font-normal text-gray-600 break-keep max-sm:text-text-m">{process}</p>
           </div>
         </div>
       </div>
       <div className="flex gap-2"></div>
-      <Image src={FreeTag} alt="무료접종" />
+      <p>무료접종</p>
     </div>
   );
 };
