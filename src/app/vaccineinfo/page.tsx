205ad8b93ed7304ea9966d684ad331bd27d60ed3
seokwon <tjrtkd95@gmail.com>
2024-11-12 15:29:02 +0900

    style: 접종 페이지 반응형 적용

--- src/app/vaccineinfo/page.tsx
+++ src/app/vaccineinfo/page.tsx
@@ -5,11 +5,12 @@ import VaccineList from "@/components/vaccineinfo/VaccineList";
 const VaccineInfoPage = async () => {
   return (
     <div
-      className="container flex flex-col mx-auto max-w-[327px] justify-center
-    sm:max-w-[800px] sm:mt-[64px]"
+      className="container flex flex-col mx-auto justify-center max-w-[800px] mt-[64px]
+      max-sm:max-w-[327px] max-sm:mt-3
+      "
     >
-      <div className="py-[6px] mb-4 sm:hidden">
-        <p className="text-gray-800 text-xl font-semibold">예방접종</p>
+      <div className="py-[6px] mb-4 hidden max-sm:block">
+        <p className="text-gray-800 text-title-m font-semibold">예방접종</p>
       </div>
       <AgeGroup />
       <AgeFilter />
