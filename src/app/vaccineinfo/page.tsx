71acf2aa0d8337c052713ec4ded0ecc84bb5f22f
seokwon <tjrtkd95@gmail.com>
2024-11-12 20:06:29 +0900

    style: 페이지 여백 설정

--- src/app/vaccineinfo/page.tsx
+++ src/app/vaccineinfo/page.tsx
@@ -6,7 +6,7 @@ const VaccineInfoPage = async () => {
   return (
     <div
       className="container flex flex-col mx-auto justify-center max-w-[800px] mt-[64px]
-      max-sm:max-w-[327px] max-sm:mt-3
+      max-sm:mt-3 max-sm:px-6
       "
     >
       <div className="py-[6px] mb-4 hidden max-sm:block">
