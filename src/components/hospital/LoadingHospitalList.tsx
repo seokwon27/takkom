925bc4022d35f4014f4f03f88d8c66d690176e81
joycie416 <joycie416@gmail.com>
2024-11-13 14:53:34 +0900

    fix: 빌드 오류 해결

--- src/components/hospital/LoadingHospitalList.tsx
+++ src/components/hospital/LoadingHospitalList.tsx
@@ -4,19 +4,25 @@ import React, { ReactNode } from "react";
 import HospitalLoading from "../../../public/common/search-takkomi.svg";
 import LoadingSpinner from "../../../public/common/loading-spinner.svg";
 
-const LoadingHospitalList = ({ children, className, animate }: { children?: ReactNode; className?: string, animate?: string }) => {
+const LoadingHospitalList = ({
+  children,
+  className,
+  animate
+}: {
+  children?: ReactNode;
+  className?: string;
+  animate?: string;
+}) => {
   return (
     <div className={cn("flex flex-col items-center mt-4 mb-16 text-title-xl", className)}>
-      {/* {children ? ( */}
-        <>
-          <Image src={HospitalLoading} alt="로딩중입니다." className={cn("max-sm:w-[160px] max-sm:aspect-sqare", animate)}/>
-          <div className="w-full mt-2 text-title-xl font-semibold text-gray-200 text-center max-sm:text-heading-xs">{children}</div>
-        </>
-      {/* ) : (
-        <div className="w-[260px] h-[260px] flex justify-center">
-          <Image src={LoadingSpinner} alt="로딩중입니다." className="animate-spin" />
-        </div>
-      )} */}
+      <Image
+        src={!!children ? HospitalLoading : LoadingSpinner}
+        alt="로딩중입니다."
+        className={cn("max-sm:w-[160px] max-sm:aspect-sqare", animate)}
+      />
+      <div className="w-full mt-2 text-title-xl font-semibold text-gray-200 text-center max-sm:text-heading-xs">
+        {children}
+      </div>
     </div>
   );
 };
--- src/components/layout/DesktopLayout.tsx
+++ src/components/layout/DesktopLayout.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import React, { PropsWithChildren } from "react";
 
 const DesktopLayout = ({ children, className }: PropsWithChildren & {className?: string}) => {
-  return <div className={cn("max-sm:hidden")}>{children}</div>;
+  return <div className={cn("max-sm:hidden", className)}>{children}</div>;
 };
 
 export default DesktopLayout;
