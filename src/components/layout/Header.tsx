0d6e7c1c900ade8795f97f1c41bd6842c3ebdcd0
Sehee Jang <seheejang94@gmail.com>
2024-11-13 12:11:01 +0900

    style: 모바일 네비게이션 스타일 적용, 헤더에 따꼼 로고 나오게 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,7 +18,7 @@ const Header = async () => {
     //   <HeaderAuth />
     // </header>
     <>
-      {/* 기존 헤더 - 모바일에서 숨기기 */}
+      {/* pc 화면세어 보이는 헤더 - 모바일에서 숨기기 */}
       <header
         className={`w-full max-w-[1200px] flex justify-between items-center mx-auto pt-12 pb-4 sticky top-0 left-0 right-0 z-10 bg-white hidden md:flex`}
       >
@@ -29,8 +29,15 @@ const Header = async () => {
         <HeaderAuth />
       </header>
 
-      {/* 모바일 하단 네비게이션 */}
-      <MobileNav />
+      {/* 모바일 환경에서 로고 상단에 배치, 모바일 하단 네비게이션 */}
+      <div className="md:hidden">
+        <header className="w-full bg-white pt-3 px-6 sticky top-0 left-0 right-0 z-10">
+          <Link href={"/"} className="flexr">
+            <Image src={logo} alt="로고" width={92} height={32} />
+          </Link>
+        </header>
+        <MobileNav />
+      </div>
     </>
   );
 };
