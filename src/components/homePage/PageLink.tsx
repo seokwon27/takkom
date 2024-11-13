d0598a0382264084e0f66af5b87e9c3fa9f15a57
seokwon <tjrtkd95@gmail.com>
2024-11-13 11:13:27 +0900

    style: 홈페이지 가로여백 설정 및 페이지링크 디자인 일부 적용

--- src/components/homePage/PageLink.tsx
+++ src/components/homePage/PageLink.tsx
@@ -11,28 +11,33 @@ import pill from "../../../public/homepage/pill.svg";
 
 const PageLink = () => {
   return (
-    <div className="flex gap-6 mt-[77px] w-full max-w-[1200px] m-auto">
+    <div className="flex gap-6 mt-[77px] w-full max-w-[1200px] m-auto max-sm:mx-6">
       <Link
         href={"/vaccineinfo"}
-        className="flex bg-[url('/homepage/vaccineInfo-bg.svg')] px-12 py-20 rounded-3xl text-white w-[736px] h-[472px] relative"
+        className="flex bg-[url('/homepage/vaccineInfo-bg.svg')] bg-cover px-12 py-20 rounded-2xl text-white w-[736px] h-[472px] relative max-sm:p-5"
       >
         <Image src={linkPerson} alt="사람아이콘" className="absolute -bottom-12 right-11" />
         <div className="flex flex-col justify-start gap-6 pt-6">
           <div>
-            <p className="text-[32px] font-bold">지금 아이에게 필요한</p>
-            <p className="text-[40px] font-bold">예방 접종 정보 확인하기</p>
+            <p className="text-heading-xxl font-bold max-sm:hidden">지금 아이에게 필요한</p>
+            <p className="text-heading-xxxl font-bold max-sm:text-heading-s max-sm:break-keep">
+              예방 접종 정보 확인하기
+            </p>
           </div>
-          <div className="flex justify-center gap-[6px] bg-white text-primary-400 max-w-[135px] p-2 rounded-[50px] text-xl font-bold">
+          <div
+            className="flex justify-center gap-[6px] bg-white text-primary-400 max-w-[135px] p-2 rounded-[50px] text-xl font-bold max-sm:w-[78px] max-sm:h-[26px]
+          max-sm:py-1 max-sm:gap-1"
+          >
             <div>
-              <Image src={searchIcon} alt="돋보기" />
+              <Image src={searchIcon} alt="돋보기" className="max-sm:w-4 max-sm:h-4" />
             </div>
-            <p>보러가기</p>
+            <p className="text-heading-m max-sm:text-heading-xxxs">보러가기</p>
           </div>
         </div>
       </Link>
 
       <div className="flex flex-col gap-6 ">
-        <Link className="p-[40px] rounded-3xl w-[440px] h-[224px] bg-[#F0F9FF] relative" href={"/child"}>
+        <Link className="p-[40px] rounded-3xl  bg-[#F0F9FF] relative" href={"/child"}>
           <Image src={handHeart} alt="아이콘" className="absolute right-0" />
           <div className="flex flex-col gap-3">
             <p className="text-2xl text-gray-800 font-bold "> 우리 아이 접종 관리하기</p>
@@ -40,7 +45,7 @@ const PageLink = () => {
               아이 맞춤형 접종 정보와 <br />
               접종 내역을 체크할 수 있어요!
             </p>
-            <div className="flex gap-2 justify-center items-center border-[1px] border-primary-300 rounded-[19px] w-[103px] h-[38px]">
+            <div className="flex gap-2 justify-center items-center border-[1px] border-primary-300 rounded-3xl w-[103px] h-[38px]">
               <Image src={addCircle} alt="등록아이콘" />
               <p className="text-primary-300 font-semibold">등록하기</p>
             </div>
@@ -48,9 +53,9 @@ const PageLink = () => {
         </Link>
         <Link
           href={"/hospital"}
-          className="flex justify-center items-center px-9 w-[440px] h-[224px] rounded-3xl border-[0.6px] border-gray-30 shadow-[0px_0px_16px_0px_rgba(114,114,114,0.10)] relative"
+          className="flex justify-center items-center px-9 py-10 rounded-3xl border-[0.6px] border-gray-30 shadow-[0px_0px_16px_0px_rgba(114,114,114,0.10)] relative"
         >
-          <div className="flex w-full justify-between">
+          <div className="flex w-full justify-between py-5">
             <div className="flex flex-col justify-center gap-4">
               <div>
                 <p className="text-[#008F5D] text-lg font-semibold">어디에서 접종 하나요?</p>
