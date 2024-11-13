f6a5b4a7cb7043c2eee4d4de7c4942518f9b69f9
Sehee Jang <seheejang94@gmail.com>
2024-11-13 12:33:56 +0900

    del: Header 컴포넌트에서 따꼼로고 제거 -> 메인에서 보여줄 예정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ import MobileNav from "./MobileNav";
 
 const Header = async () => {
   return (
-    <>
+    <>    
       {/* pc 화면세어 보이는 헤더 - 모바일에서 숨기기 */}
       <header
         className={`w-full max-w-[1200px] flex justify-between items-center mx-auto pt-12 pb-4 sticky top-0 left-0 right-0 z-10 bg-white hidden md:flex`}
@@ -22,11 +22,11 @@ const Header = async () => {
 
       {/* 모바일 환경에서 로고 상단에 배치, 모바일 하단 네비게이션 */}
       <div className="md:hidden">
-        <header className="w-full bg-white pt-3 px-6 sticky top-0 left-0 right-0 z-10">
+        {/* <header className="w-full bg-white pt-3 px-6 sticky top-0 left-0 right-0 z-10">
           <Link href={"/"} className="flexr">
             <Image src={logo} alt="로고" width={92} height={32} />
           </Link>
-        </header>
+        </header> */}
         <MobileNav />
       </div>
     </>
