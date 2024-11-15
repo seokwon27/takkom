8d5fcbbe3f367a53682817a984de33de41a2bef9
seokwon <tjrtkd95@gmail.com>
2024-11-15 15:00:01 +0900

    add: pwa 기능 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/rootlayout/Header";
 import Footer from "@/components/rootlayout/Footer";
 import Image from "next/image";
 import takkomi from "../../public/homepage/takkomi.svg";
+import { PWAProvider } from "./PWAProvider";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -17,8 +18,10 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: "따꼼",
   description: "우리 아이 예방 접종 정보를 저장하고 무료 접종 병원을 찾을 수 있습니다.",
+  manifest: "/manifest.json",
   icons: {
-    icon: "/favicon.svg"
+    icon: "/favicon.svg",
+    apple: "/icon/icon-192x192.png"
   }
 };
 
@@ -29,6 +32,11 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="kr">
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="내 PWA 앱" />
+      </head>
       <body className={`${pretendard.className} flex flex-col min-h-screen relative`}>
         <Providers>
           <Header />
@@ -37,6 +45,7 @@ const RootLayout = ({
           </main>
           <Image src={takkomi} alt="따꼼빼꼼" className="absolute right-[120px] bottom-[345px] z-10 max-sm:hidden" />
           <Footer />
+          <PWAProvider />
         </Providers>
       </body>
     </html>
