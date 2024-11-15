131266adb66baca6e1660d2b2e2dec5f1be08b31
joycie416 <joycie416@gmail.com>
2024-11-15 10:02:05 +0900

    style: 푸터 고정폭 해제

--- src/components/rootlayout/Footer.tsx
+++ src/components/rootlayout/Footer.tsx
@@ -4,9 +4,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flex flex-col w-full mx-auto items-center justify-center gap-2.5 relative bg-[#f7f9fa] max-sm:hidden">
-      <div className="flex flex-col max-w-[1200px] max-h-[480px] items-start gap-12 p-10 relative bg-[#f7f9fa]">
+      <div className="flex flex-col w-full max-w-[1200px] max-h-[480px] items-start gap-12 p-10 relative bg-[#f7f9fa]">
         <div className="flex items-start justify-between relative self-stretch w-full flex-[0_0_auto]">
           <div className="flex flex-col w-[272px] items-start gap-3 relative">
+            {/* 고객센터 */}
             <div className="flex flex-col items-start gap-3 relative self-stretch w-full flex-[0_0_auto]">
               <div className="self-stretch mt-[-1.00px] font-heading-s font-[number:var(--heading-s-font-weight)] text-gray-800 text-[length:var(--heading-s-font-size)] leading-[var(--heading-s-line-height)] relative tracking-[var(--heading-s-letter-spacing)] [font-style:var(--heading-s-font-style)]">
                 고객센터
@@ -24,7 +25,8 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="inline-flex items-start gap-[120px] relative flex-[0_0_auto]">
+          <div className="w-full max-w-[568px] min-w-[400px] inline-flex justify-between items-start relative flex-[0_0_auto]">
+            {/* Information */}
             <div className="flex flex-col w-[115px] h-[187px] items-start gap-6 relative">
               <div className="relative self-stretch mt-[-1.00px] font-heading-XS font-[number:var(--heading-XS-font-weight)] text-gray-600 text-[length:var(--heading-XS-font-size)] tracking-[var(--heading-XS-letter-spacing)] leading-[var(--heading-XS-line-height)] [font-style:var(--heading-XS-font-style)]">
                 Information
@@ -45,6 +47,7 @@ const Footer = () => {
               </div>
             </div>
 
+            {/* Service */}
             <div className="flex flex-col w-[75px] h-[115px] items-start gap-6 relative">
               <div className="relative self-stretch mt-[-1.00px] font-heading-XS font-[number:var(--heading-XS-font-weight)] text-gray-600 text-[length:var(--heading-XS-font-size)] tracking-[var(--heading-XS-letter-spacing)] leading-[var(--heading-XS-line-height)] [font-style:var(--heading-XS-font-style)]">
                 Service
@@ -61,6 +64,7 @@ const Footer = () => {
               </div>
             </div>
 
+            {/* Category */}
             <div className="flex flex-col w-[138px] h-[151px] items-start gap-6 relative">
               <div className="relative self-stretch mt-[-1.00px] font-heading-XS font-[number:var(--heading-XS-font-weight)] text-gray-600 text-[length:var(--heading-XS-font-size)] tracking-[var(--heading-XS-letter-spacing)] leading-[var(--heading-XS-line-height)] [font-style:var(--heading-XS-font-style)]">
                 Category
@@ -94,27 +98,53 @@ const Footer = () => {
 
         <div className="flex flex-col items-start gap-6 relative self-stretch w-full flex-[0_0_auto]">
           <div className="flex flex-col items-start gap-2 relative self-stretch w-full flex-[0_0_auto]">
-            <div className="flex items-center gap-4 relative self-stretch w-full flex-[0_0_auto]">
-              <div className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+            <p className="flex items-center gap-4 relative self-stretch w-full flex-[0_0_auto] line-clamp-2">
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
                 (주)따꼼 컴퍼니
-              </div>
+              </span>
 
-              <div className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
-                대표
-              </div>
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                만든이
+              </span>
 
-              <div className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
-                윤창식
-              </div>
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                이석원
+              </span>
 
-              <div className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
-                설재혁
-              </div>
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                조해인
+              </span>
 
-              <p className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                이예람
+              </span>
+
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                장세희
+              </span>
+
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                정지형
+              </span>
+
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                전수빈
+              </span>
+
+              <span className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
                 서울특별시 강남구 테헤란로 415, 4층(따꼼동, 엘7강남타워)
-              </p>
+              </span>
 
+              {/* <div className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
+                대표전화 : 1800-따꼼따꼼
+              </div>
+
+              <div className="relative w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] tracking-[var(--label-XL-letter-spacing)] leading-[var(--label-XL-line-height)] whitespace-nowrap [font-style:var(--label-XL-font-style)]">
+                이메일 : ddaggom@ddaggom.co.kr
+              </div> */}
+            </p>
+
+            <div className="w-fit flex gap-[35px]">
               <div className="w-fit mt-[-1.00px] font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] leading-[var(--label-XL-line-height)] whitespace-nowrap relative tracking-[var(--label-XL-letter-spacing)] [font-style:var(--label-XL-font-style)]">
                 대표전화 : 1800-따꼼따꼼
               </div>
@@ -135,7 +165,7 @@ const Footer = () => {
             </div>
 
             <p className="relative self-stretch font-label-XL font-[number:var(--label-XL-font-weight)] text-gray-600 text-[length:var(--label-XL-font-size)] tracking-[var(--label-XL-letter-spacing)] leading-[var(--label-XL-line-height)] [font-style:var(--label-XL-font-style)]">
-              따꼼은 통신판매중개잘로서 중개하는 거래에 대하여 책임을 부담하지 않습니다.
+              따꼼은 통신판매중개자로서 중개하는 거래에 대하여 책임을 부담하지 않습니다.
             </p>
           </div>
         </div>
