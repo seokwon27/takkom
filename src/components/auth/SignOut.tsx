d72f06a8817d1ba489ca7d5417788ee710d4ac55
이예람 <killhvhl1129@naver.com>
2024-11-20 04:28:43 +0900

    refactor : 로그아웃 컴포넌트 주석 제거

--- src/components/auth/SignOut.tsx
+++ src/components/auth/SignOut.tsx
@@ -2,26 +2,18 @@ import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 import { signout } from "@/api/auth-actions";
 import { useQueryClient } from "@tanstack/react-query";
-// import { useState } from "react";
-// import SignOutModal from "./SignOutModal";
 
 const SignOut = () => {
   const router = useRouter();
   const queryClient = useQueryClient();
-  // const [showModal, setShowModal] = useState(false); // 모달 상태 관리
 
   const handleSignOut = async () => {
     queryClient.invalidateQueries({ queryKey: ["user"] });
 
     await signout();
 
-    //   setShowModal(false); // 로그아웃 후 모달 닫기
     router.push("/");
   };
-
-  // const handleCloseModal = () => {
-  //   setShowModal(false); // 모달 닫기
-  // };
 
   return (
     <>
@@ -32,13 +24,6 @@ const SignOut = () => {
       >
         로그아웃
       </Button>
-
-      {/* SignOutModal 컴포넌트 사용 */}
-      {/* <SignOutModal
-        showModal={showModal}
-        onClose={handleCloseModal} // 취소 버튼이나 모달 외부 클릭 시 모달 닫기
-        onSignOut={handleSignOut} // 로그아웃 처리
-      /> */}
     </>
   );
 };
