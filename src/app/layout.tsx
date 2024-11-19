9dec7efbb1e20291894afec23e75b8e07d0669e0
seokwon <tjrtkd95@gmail.com>
2024-11-19 20:35:40 +0900

    fix: html 공백 경고로 인한 주석 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,12 +32,11 @@ const RootLayout = ({
   return (
     <html lang="kr">
       <head>
-        {/* iOS용 PWA 메타테그 */}
-        <meta name="apple-mobile-web-app-capable" content="yes" /> {/* PWA가 전체화면으로 실행되도록 설정 */}
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" /> {/* iOS 상태바의 스타일 */}
-        <meta name="apple-mobile-web-app-title" content="따꼼" /> {/* 홈 화면에 추가될 때 표시될 앱의 이름 */}
-        <link rel="apple-touch-icon" href="/icon/icon-512x512.png" /> {/* 앱 아이콘 지정 */}
-        <meta name="format-detection" content="telephone=no" /> {/* PWA가 링크처럼 보이는 것 방지 */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="따꼼" />
+        <link rel="apple-touch-icon" href="/icon/icon-512x512.png" />
+        <meta name="format-detection" content="telephone=no" />
       </head>
       <body className={`${pretendard.className} flex flex-col min-h-screen relative`}>
         <Providers>
@@ -45,7 +44,7 @@ const RootLayout = ({
           <main className="grow flex flex-col w-full max-w-[1200px] mx-auto mb-[100px] max-sm:mx-0 max-sm:mb-0 ">
             {children}
           </main>
-          <Toaster/>
+          <Toaster />
           <Footer />
           <PWAProvider />
         </Providers>
