f6bee9ac888af6ac418488530f391429170a2fbb
joycie416 <joycie416@gmail.com>
2025-01-21 18:45:57 +0900

    fix: 불필요한 글자 제거

--- src/components/hospitalDetail/SearchParams.tsx
+++ src/components/hospitalDetail/SearchParams.tsx
@@ -1,14 +1,12 @@
-'use client'
+"use client";
 
 import useQueryParams from "@/hooks/use-query-param";
-import { HospitalSearchParams } from "@/types/hospital"
+import { HospitalSearchParams } from "@/types/hospital";
 
-const SearchParams = ({searchParams} : {searchParams: HospitalSearchParams}) => {
+const SearchParams = ({ searchParams }: { searchParams: HospitalSearchParams }) => {
   useQueryParams(new URLSearchParams(searchParams).toString());
-  
-  return (
-    <div>SearchParams</div>
-  )
-}
 
-export default SearchParams
+  return <></>;
+};
+
+export default SearchParams;
